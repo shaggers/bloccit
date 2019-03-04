9f6da85062491f1b86dc4fdcaf668a1493b880ca
shaggers <camtl66@gmail.com>
2019-03-04 16:24:01 -0600

    completed checkpoint 5

--- spec/integration/topics_spec.js
+++ spec/integration/topics_spec.js
@@ -2,7 +2,7 @@ const request = require("request");
 const server = require("../../src/server");
 const base = "http://localhost:3000/topics/";
 const sequelize = require("../../src/db/models/index").sequelize;
-const Topic = require("../../src/db/models").Topic;
+const Topic = require("../../src/db/models").Topics;
 
 describe("routes : topics", () => {
 

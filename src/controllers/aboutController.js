609e373068ab021283bf49f26144bcd3ccfd7389
shaggers <camtl66@gmail.com>
2019-03-04 18:43:08 -0600

    trying to render the about partial

--- src/controllers/aboutController.js
+++ src/controllers/aboutController.js
@@ -1,5 +1,8 @@
 module.exports = {
     index(req, res, next){
       res.send("About Us");
+      next()
+    }, function (req, res){
+        res.render("static/index", {title: "About Us"});
     }
 }

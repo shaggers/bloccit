4f000152898a593d59e26ba7aaf78c228327a388
shaggers <camtl66@gmail.com>
2019-03-06 14:50:44 -0600

    completed assignment4

--- src/app.js
+++ src/app.js
@@ -4,9 +4,9 @@ const app = express();
 const appConfig = require("./config/main-config.js");
 const routeConfig = require("./config/route-config.js");
 
-routeConfig.init(app);
-
 appConfig.init(app, express);
+
+routeConfig.init(app);
 
 module.exports = app;
 
--- src/controllers/aboutController.js
+++ src/controllers/aboutController.js
@@ -1,8 +1,5 @@
 module.exports = {
     index(req, res, next){
-      res.send("About Us");
-      next()
-    }, function (req, res){
-        res.render("static/index", {title: "About Us"});
+      res.render("about/index", {title: "About Us"});
     }
 }

89c5f1dd82e916c0d7a06a6cb3cb98953721d940
shaggers <camtl66@gmail.com>
2019-03-18 22:25:37 -0500

    assignment 15 completed

--- src/db/models/post.js
+++ src/db/models/post.js
@@ -52,6 +52,14 @@ module.exports = (sequelize, DataTypes) => {
         postId: post.id
       });
     });
+    
+    Post.afterCreate((post, callback) => {
+      return models.Votes.create({
+        value: 1,
+        userId: post.userId,
+        postId: post.id
+      });
+    });
   };
 
   Post.prototype.getPoints = function(){
